54fcc7bb8d8b83bb1ce841d57e142035747d6e3a
laishi <laishi886@hotmail.com>
2021-01-23 17:55:51 +0800

    jiuba

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get('/', function(req, res) {
     res.sendFile('/index.html');
 });
 
-app.listen(80);
+app.listen(9898);
 
-console.log("http://148.70.212.82:80/");
+console.log("http://148.70.212.82:9898/");
